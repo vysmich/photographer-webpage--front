b4a17bbb3aea8b0c969e5ece5f4ea2dd31c4c8df
Michal Vyskočil <michal.vyskocil@q2.cz>
2022-03-23 23:23:16 +0100

    added i18n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  i18n: {
+    // providing the locales supported by your application
+    locales: ["cs-CZ", "en-US"],
+    //  default locale used when the non-locale paths are visited
+    defaultLocale: "cs-CZ",
+  },
+};
