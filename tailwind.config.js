05e7f885b133f49a2d263e9e689a6d5b5b304774
Michal Vyskočil <michal.vyskocil@q2.cz>
2022-03-25 21:07:28 +0100

    Set colors, fonts and breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
-    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1400px",
+      xxxl: "1920px",
     },
-    plugins: [],
+    colors: {
+      primary: "#ad8965",
+      secondary: "#d6cbb7",
+      light: "#d6cbb7",
+      dark: "#945532",
+    },
+
+    extend: {
+      fontFamily: {
+        ptserif: ["PT Serif", "serif"],
+        quitcher: ["Qwitcher Grypen", "cursive"],
+      },
+    },
+  },
 };
