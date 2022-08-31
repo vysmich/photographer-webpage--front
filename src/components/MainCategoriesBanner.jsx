d33d706adc26f81f0a94fc0027d70cbf90f83f2f
Michal Vyskočil <michal.vyskocil@q2.cz>
2022-09-01 00:02:13 +0200

    add animation

--- src/components/MainCategoriesBanner.jsx
+++ src/components/MainCategoriesBanner.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
+import { Fade } from "react-awesome-reveal";
 
 import BorderImg from "./BorderImg";
 
@@ -18,16 +19,18 @@ function MainCategoriesBanner({ gallery, title, portfoliotBtn, content }) {
           </Link>
         </div>
         <div className=" grid gap-5 xl:grid-cols-4 ">
-          {gallery.map((item) => (
-            <div key={item.CategoryName} className="relative h-[500px]">
-              <BorderImg
-                imgUrl={item.Image.data.attributes.url}
-                imgText={item.CategoryName}
-                imgAlt={item.Image.data.attributes.alternativeText}
-                imgLink={item.Link}
-              />
-            </div>
-          ))}
+          <Fade cascade direction={"left"} duration={700}>
+            {gallery.map((item) => (
+              <div key={item.CategoryName} className="relative h-[500px]">
+                <BorderImg
+                  imgUrl={item.Image.data.attributes.url}
+                  imgText={item.CategoryName}
+                  imgAlt={item.Image.data.attributes.alternativeText}
+                  imgLink={item.Link}
+                />
+              </div>
+            ))}
+          </Fade>
         </div>
       </div>
     </section>
