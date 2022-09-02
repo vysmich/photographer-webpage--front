8ed56ef27880683c391b3f623daa4374b8555ff1
Michal Vyskočil <michal.vyskocil@q2.cz>
2022-09-02 23:27:41 +0200

    fix mobile header

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -22,7 +22,7 @@ function Layout({ children, contextLocale, layoutData, hero, data }) {
     <div>
       <header
         className={
-          "fixed z-10 flex w-full translate-y-0 flex-row-reverse items-center bg-bgsecondary transition duration-700 ease-in-out xl:flex-row " +
+          "z-10 flex w-full translate-y-0 flex-row-reverse items-center bg-bgsecondary transition duration-700 ease-in-out xl:fixed xl:flex-row " +
           headerClass
         }
       >
